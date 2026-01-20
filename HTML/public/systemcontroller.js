933d9f4eaeb83852686c8551670dc1bc513aa715
Greimelinio09 <felix.nzgleisdorf@gmail.com>
2026-01-20 18:47:27 +0100

    usbtest

--- HTML/public/systemcontroller.js
+++ HTML/public/systemcontroller.js
@@ -1,3 +1,21 @@
 // Globales Array für alle eingegangenen Bestellungen
 let submittedOrders = [];
 
+let oderqueque = [];
+
+if(submittedOrders[0] != '')
+{
+   import { SerialPort } from 'serialport';
+   
+   const port = new SerialPort({
+    path: '/dev/ttyUSB0',
+    baudRate: 115200});
+
+
+    port.write("Hello from Node.js!\n", (err) => {
+      if (err) {
+        return console.log('Error on write: ', err.message);
+      }
+      console.log('message written');
+    });
+}
