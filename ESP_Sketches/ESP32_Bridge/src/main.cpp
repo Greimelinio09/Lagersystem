0a72822a9b6248d23ac505372f54ca08f2e93f81
Greimelinio09 <felix.nzgleisdorf@gmail.com>
2026-02-01 18:51:17 +0100

    working sentence communication

--- ESP_Sketches/ESP32_Bridge/src/main.cpp
+++ ESP_Sketches/ESP32_Bridge/src/main.cpp
@@ -2,13 +2,31 @@
 
 #define LED_PIN 2
 
+bool getSerialdata();
+void ledTest(){
+  digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+  delay(1000);
+}
+
 void setup() {
+  Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
+  bool ledState = getSerialdata();
+  //ledTest();
+
+}
+
+bool getSerialdata() {
+  if(Serial.available() > 0)
+    {
+      String input = Serial.readStringUntil('\n'); // Read input until newline character
+      Serial.print("Received: ");
+      Serial.println(input); // Echo the received input back to Serial Monitor
+    }
+  
+  
+  return false; // default return value if no complete string received
 }
